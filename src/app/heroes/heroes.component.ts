c7006362ed335f37f345e881ba68fa6f3b051f9d
Martin Plieske <martin.plieske@gmail.com>
2021-07-19 23:53:00 +0200

    Add Navigation: new navigation for Hero Detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,17 +13,11 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id='${hero.id}'`)
   }
 
   getHeroes(): void {
